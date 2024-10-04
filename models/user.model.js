1b38c3b5096cdf861ebb2476b5f8ec8845272969
Onifade Joshua <onifadjosh@gmail.com>
2024-10-04 09:09:12 +0100

    Added email, password, and date_created fields to UserSchema

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 
 const UserSchema = mongoose.Schema({
-    fullname :{type:String, require:true}
+    fullname :{type:String, require:true},
+    email :{type:String, require:true},
+    password :{type:String, require:true},
+    date_created : {
+        type: Date, default:Date.now()
+    }
 })
 
 const UserModel = mongoose.model('user_collection', UserSchema)
